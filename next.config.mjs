700fa1bd073b42861ab548835d4211662fdcf7a0
k2sebeom <moses97@gmail.com>
2024-02-13 17:43:44 +0900

    Feat: Add dockerfiles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // standalone 모드
+};
 
 export default nextConfig;
